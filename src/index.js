31736da34ccaec66aba951a24a4dbe1b4dea805e
Ere-yiga <erehdagr8@gmail.com>
2025-06-01 21:48:37 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App(){
-    return<h1>Hello react!!</h1>
+    return<h1>Hello react</h1>
 }
 //React V18
 const root = ReactDOM.createRoot(document.getElementById ("root"));
