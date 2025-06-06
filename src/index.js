3ebb455fac936daf9c6f50d98b794b65ac22626c
Ere-yiga <erehdagr8@gmail.com>
2025-06-06 08:27:42 +0100

    adding more components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,92 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+const PizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App(){
   return(
     <div>
-      <h1>Hello react</h1>
-      <button>Click Me</button>
+      <Header />
+    
+      <Menu />
+      <Pizza/>
+      <Pizza/>
+      <Pizza/>
+      <Pizza/>
+      <Pizza/>
+      <Pizza/>
+      <Footer />
     </div>
   );
 }
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-  alert("welcome")
-});
+function Header(){
+  return(
+    <h1>Hey there, you're welcome to Ereh's first React livepage</h1>
+  )
+}
+function Menu(){}
+
+function Pizza(){
+  return(
+    <div>
+      <img src="/pizzas/spinaci.jpg" alt="pizza spinachi" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  )
+};
+
+function Footer(){
+  return <p></p> 
+  //React.createElement("footer", null, "Welcome peeps");
+}
 const root = ReactDOM.createRoot(document.getElementById ("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
 ); 
