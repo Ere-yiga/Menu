e20333d1faa05c525bb9a00cc035bd10f41d5861
Ere-yiga <erehdagr8@gmail.com>
2025-06-07 23:56:21 +0100

    pushing

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ const PizzaData = [
 
 function App(){
   return(
-    <div>
+    <div className = "container">
       <Header />
       <Menu />
       <Footer />
@@ -57,46 +57,95 @@ function App(){
   );
 }
 function Header(){
- const style = {color: "blue", fontSize: "40px"};
-  return(
-    <h1 style={style}>Hey there, you're welcome to Ereh's first React livepage</h1>
+ //const style = {color: "blue", fontSize: "40px"};
+ const style = {} 
+ return(
+    <header className="header">
+      <h1 style={style}>Pizza Co.</h1>
+    </header>
   )
 }
 function Menu(){
-  return (
-    <div>
-      <h2>Our Menu</h2>
-      <Pizza/>
-      <Pizza/>
-      <Pizza/>
-      <Pizza/>
-      <Pizza/>
-      <Pizza/>
-    </div>
-  )
-}
+  const pizzas = PizzaData; 
+  /* const pizzas= {};*/
+  const numPizzas = pizzas.length;
 
-function Pizza(){
+  return (
+    <main className = "menu">
+      <h2>Our Menu</h2>
+
+      {numPizzas > 0 ? (
+        <>
+
+          <p>Italian</p>
+
+          <ul className="pizzas">
+            {PizzaData.map(pizza =>
+               <Pizza pizzaObj={pizza} key={pizza.name}/>)}
+          </ul>
+        </>
+      ): <p>We're still working on our menu, please come back later</p>} 
+     {/*  <Pizza name="Pizza Spinachi" ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+      photoName="pizzas/spinaci.jpg" 
+      price={10}
+      />
+
+      <Pizza name="Pizza Funghi"
+      ingredients="Tomato mushrooms"
+      price={12}
+      photoName = "pizzas/funghi.jpg"
+      /> */}
+    </main>
+  );
+}
+//INSTEAD OF USING .props everytime, I CAN JUST DESTRUCTURE
+function Pizza({ pizzaObj }){
+  //if(pizzaObj.soldOut) return null;
   return(
-    <div>
-      <img src="/pizzas/spinaci.jpg" alt="pizza spinachi" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
+    <li className="pizza">
+      <img src={pizzaObj.photoName} alt={pizzaObj.name}/>
+      <div className="">
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "Sold Out" : pizzaObj.price}</span>
+      </div>
+    </li>
   )
 };
 
 function Footer(){
   const hour = new Date().getHours();
-  const openHour = 9;
+  const openHour = 12;
   const closingHour = 22;
   const isOpen = hour >= openHour && hour <= closingHour
   console.log(isOpen)
   //if(hour >= openHour && hour <= closingHour)alert("We are opened");
   //else alert("We are closed");
 
-  return <footer>{new Date().toLocaleTimeString()} Welcome peeps</footer> 
+/*   if(!isOpen)
+    return (
+      <p>
+        We're happy to welcome you betweem {openHour}:00 and {closingHour}:00
+      </p>
+    )  */
+  
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closingHour={closingHour} openHour={openHour}/>
+      ) : <p>We're happy to welcome you betweem {openHour}:00 and {closingHour}:00</p>}
+    </footer>
+  );
   //React.createElement("footer", null, "Welcome peeps");
+}
+
+function Order({ closingHour, openHour }){
+  return(
+    <div className="order">
+      <p>We are open from {openHour}:00 to {closingHour}:00. Come visit us or order online</p>          
+      <button className="btn">Order</button>
+    </div>
+  )
 }
 const root = ReactDOM.createRoot(document.getElementById ("root"));
 root.render(
@@ -104,4 +153,4 @@ root.render(
     <App />
   </React.StrictMode>
 
-); 
+);
