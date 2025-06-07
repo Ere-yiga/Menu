8bdcee0e7a56b949a58dbcb39386080905540116
Ere-yiga <erehdagr8@gmail.com>
2025-06-08 00:03:05 +0100

    pushing

--- src/index.js
+++ src/index.js
@@ -102,7 +102,7 @@ function Menu(){
 function Pizza({ pizzaObj }){
   //if(pizzaObj.soldOut) return null;
   return(
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name}/>
       <div className="">
         <h3>{pizzaObj.name}</h3>
